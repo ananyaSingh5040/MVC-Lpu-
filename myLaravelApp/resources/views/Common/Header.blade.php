cb56bc810aef65dc6a3461c3485a7b21f72be34e
Ananya Singh <singhananya5040@gmail.com>
2025-03-22 10:02:07 +0530

    22-03-25

---
+++ myLaravelApp/resources/views/Common/Header.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<style>
+    .navBar {
+        display: flex;
+        text-align: center;
+        position: fixed;
+
+
+    }
+
+    .nav {
+
+        background-color: whitesmoke;
+        margin: 10px;
+        border-radius: 10px;
+        width: 99vw;
+
+    }
+
+    .list {
+        display: flex;
+        gap: 400px;
+        font-size: xx-large;
+    }
+
+    a {
+        text-decoration: none;
+    }
+</style>
+
+<body>
+    <div class="navBar">
+        <nav class="nav">
+            <ul class="list">
+                <a href="">Home</a>
+                <a href="">About </a>
+                <a href="">Contact</a>
+            </ul>
+        </nav>
+    </div>
+</body>
+
+</html>
