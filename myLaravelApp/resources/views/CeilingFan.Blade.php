f13af8a3af340ce46d4c8a3613a057c3ffffe0cf
Ananya Singh <singhananya5040@gmail.com>
2025-03-28 11:38:21 +0530

    28-03-25

---
+++ myLaravelApp/resources/views/CeilingFan.Blade.php
@@ -0,0 +1,4 @@
+<h1>Ceiling Fan</h1>
+<b>Price:</b> <span>2000Rs</span> <br><br>
+<b>Description:</b> <span> A working , beautifully crafted Ceiling Fan </span> <br><br>
+<a href="{{route('product')}}">Back to Product List</a>
