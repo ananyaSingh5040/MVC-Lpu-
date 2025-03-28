dc35c5ebf08cbcdfaaa8c0adb72f135a8789d4a2
Ananya Singh <singhananya5040@gmail.com>
2025-03-28 11:38:25 +0530

    28-03-25

---
+++ myLaravelApp/resources/views/ExtensionCord.blade.php
@@ -0,0 +1,4 @@
+<h1>Extension Cord</h1>
+<b>Price:</b> <span>500Rs</span> <br><br>
+<b>Description:</b> <span> 5-meter extension cord with surge protection </span><br><br>
+<a href="{{route('product')}}">Back to Product List</a>
