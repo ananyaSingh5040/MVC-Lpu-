29d2b4b8eba007e57d16886dcc5027ead447fbb2
Ananya Singh <singhananya5040@gmail.com>
2025-03-31 23:58:45 +0530

    31-03-25

---
+++ myLaravelApp/resources/views/Led.blade.php
@@ -0,0 +1,4 @@
+<h1>LED Bulb</h1>
+<b>Price:</b> <span>100Rs</span> <br><br>
+<b>Description:</b> <span> A working , beautifully crafted LED Bulb </span> <br><br>
+<a href="{{route('product')}}">Back to Product List</a>
