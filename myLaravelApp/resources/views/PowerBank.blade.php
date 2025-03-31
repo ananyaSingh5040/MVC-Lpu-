e243d84e6c598b090fa3e03338ca6fe843a13c8b
Ananya Singh <singhananya5040@gmail.com>
2025-03-31 23:58:49 +0530

    31-03-25

---
+++ myLaravelApp/resources/views/PowerBank.blade.php
@@ -0,0 +1,4 @@
+<h1>Power Bank</h1>
+<b>Price:</b> <span>1200Rs</span> <br><br>
+<b>Description:</b> <span> A working , beautifully crafted Power Bank </span><br><br>
+<a href="{{route('product')}}">Back to Product List</a>
