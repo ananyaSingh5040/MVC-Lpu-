a67a2e8627e32e172c1a5d985863e16d4db04a97
Ananya Singh <singhananya5040@gmail.com>
2025-03-31 23:58:33 +0530

    31-03-25

---
+++ myLaravelApp/resources/views/Kettle.blade.php
@@ -0,0 +1,5 @@
+<h1>Electric Kettle</h1>
+<b>Price:</b> <span>1500Rs</span> <br><br>
+<b>Description:</b> <span> A working , beautifully crafted Electric Kettle </span><br><br>
+<a href="{{route('product')}}">Back to Product List</a>
+ 
