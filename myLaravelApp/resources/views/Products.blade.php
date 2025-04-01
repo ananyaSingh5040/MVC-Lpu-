e60219fdf8a3e2fd05b18dfad9db3b99590ab8ea
Ananya Singh <singhananya5040@gmail.com>
2025-04-01 11:08:27 +0530

    1-04-25

---
+++ myLaravelApp/resources/views/Products.blade.php
@@ -0,0 +1,6 @@
+<h1>Products Avaiable at Electric Shop</h1>
+<li><a href="/products/1">LED Bulb - Rs 100</a></li>
+<li><a href="/products/2">Extension Cord - Rs 500</a></li>
+<li><a href="/products/3">Ceiling Fan - Rs 2000</a></li>
+<li><a href="/products/4">Electric Kettle - Rs 1500</a></li>
+<li><a href="/products/5">Power Bank - Rs 1200</a></li>
