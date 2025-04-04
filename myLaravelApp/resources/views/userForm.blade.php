d7147f6933a32a67115ea87c332e308c4fcbafaf
Ananya Singh <singhananya5040@gmail.com>
2025-04-04 10:28:35 +0530

    4-04-25

---
+++ myLaravelApp/resources/views/userForm.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="username">Name: </label> 
+        <input type="text" name= "username" id="username" placeholder="Enter your name..." value="">
+        <label for="password">Password: </label> 
+        <input type="password" name= "password" id="password" placeholder="Enter your password..." value="">
+    </form>
+
+</body>
+</html>
