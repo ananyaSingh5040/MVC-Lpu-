8002b73359c5862ba4bd3069f22b06439fac6e99
Ananya Singh <singhananya5040@gmail.com>
2025-04-13 22:32:24 +0530

    13-04-25

--- myLaravelApp/resources/views/userForm.blade.php
+++ myLaravelApp/resources/views/userForm.blade.php
@@ -6,11 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <!-- {{print_r($errors)}} -->
+     <!-- @if($errors->any())
+     @foreach($errors->all() as $error)
+     <p>{{$error}}</p>
+     @endforeach
+     @endif -->
+    <form action="/usercont" method="post">
+        <!-- CROSS SITE REQUEST FORGERY -->
+        @csrf
         <label for="username">Name: </label> 
-        <input type="text" name= "username" id="username" placeholder="Enter your name..." value="">
+        <input type="text" name= "username" id="username" placeholder="Enter your name..." value="{{old('username')}}">
+        <span style="color: red;">@error('username'){{$message}}@enderror</span> <br><br>
         <label for="password">Password: </label> 
-        <input type="password" name= "password" id="password" placeholder="Enter your password..." value="">
+        <input type="password" name= "password" id="password" placeholder="Enter your password..." value="{{old('password')}}">
+        <!-- Security reasons ke wajah se the password is not inPlace -->
+        <span style="color: red;">@error('password'){{$message}}@enderror</span> <br><br>
+        <button type="Submit">Submit</button>
     </form>
 
 </body>
